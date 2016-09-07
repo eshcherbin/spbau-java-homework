b4685f541843c653071e745f11a301fbad777dce
Egor Lord_F Shcherbin <javamanse@ya.ru>
2016-09-08 00:25:48 +0700

    Initial commit

---
+++ src/ru/spbau/eshcherbin/java/homework/Main.java
@@ -0,0 +1,8 @@
+package ru.spbau.eshcherbin.java.homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, this is my homework project!");
+    }
+}
