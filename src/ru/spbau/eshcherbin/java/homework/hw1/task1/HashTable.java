00f1e02a9568a7b327770e469385bc70f35ac8ba
Egor Lord_F Shcherbin <javamanse@ya.ru>
2016-09-12 17:33:15 +0700

    First realisation of HashTable and LinkedList

--- src/ru/spbau/eshcherbin/java/homework/hw1/task1/HashTable.java
+++ src/ru/spbau/eshcherbin/java/homework/hw1/task1/HashTable.java
@@ -1,4 +1,88 @@
 package ru.spbau.eshcherbin.java.homework.hw1.task1;
 
 public class HashTable {
+    public static class Entry {
+        String key,
+               value;
+
+        public Entry(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private static final int INITIAL_TABLE_SIZE = 4;
+
+    private int size;
+    private LinkedList[] table;
+
+    public HashTable() {
+        size = 0;
+        table = new LinkedList[INITIAL_TABLE_SIZE];
+    }
+
+    public HashTable(int tableSize) {
+        size = 0;
+        table = new LinkedList[tableSize];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean contains(String key) {
+        int index = getIndex(key);
+        return table[index].contains(key);
+    }
+
+    public String get(String key) {
+        int index = getIndex(key);
+        return table[index].get(key);
+    }
+
+    public String put(String key, String value) {
+        return put(new Entry(key, value));
+    }
+
+    public String put(Entry entry) {
+        int index = getIndex(entry.key);
+        String result = table[index].put(entry);
+        if (result == null) {
+            size++;
+            if (size == table.length)
+                extendTable();
+        }
+        return result;
+    }
+
+    public String remove(String key) {
+        int index = getIndex(key);
+        String result = table[index].remove(key);
+        if (result != null)
+            size--;
+        return result;
+    }
+
+    public void clear() {
+        size = 0;
+        for (LinkedList list : table)
+            list.clear();
+    }
+
+    private int getIndex(String string) {
+        int index = string.hashCode() % table.length;
+        if (index < 0)
+            index += table.length;
+        return index;
+    }
+
+    private void extendTable() {
+        HashTable biggerTable = new HashTable(table.length * 2);
+        for (LinkedList list : table)
+            while (!list.empty()) {
+                Entry entry = list.popHead();
+                biggerTable.put(entry);
+            }
+        table = biggerTable.table;
+    }
 }
---
+++ src/ru/spbau/eshcherbin/java/homework/hw1/task1/LinkedList.java
@@ -0,0 +1,78 @@
+package ru.spbau.eshcherbin.java.homework.hw1.task1;
+
+public class LinkedList {
+    private static class ListItem {
+        ListItem next;
+        HashTable.Entry data;
+
+        public ListItem(ListItem next, HashTable.Entry data) {
+            this.next = next;
+            this.data = data;
+        }
+
+        public ListItem(HashTable.Entry data) {
+            this(null, data);
+        }
+    }
+
+    private ListItem head;
+
+    public boolean contains(String key) {
+        ListItem item = head;
+        while (item != null)
+            if (item.data.key.equals(key))
+                return true;
+        return false;
+    }
+
+    public String get(String key) {
+        ListItem item = head;
+        while (item != null)
+            if (item.data.key.equals(key))
+                return item.data.value;
+        return null;
+    }
+
+    public String put(HashTable.Entry entry) {
+        ListItem currentItem = head;
+        while (currentItem != null)
+            if (currentItem.data.key.equals(entry.key)) {
+                String oldValue = currentItem.data.value;
+                currentItem.data.value = entry.value;
+                return oldValue;
+            }
+        head = new ListItem(head, entry);
+        return null;
+    }
+
+    public String remove(String key) {
+        ListItem dummyItem = new ListItem(head, null),
+                 currentItem = dummyItem;
+        while (currentItem.next != null) {
+            if (currentItem.next.data.key.equals(key)) {
+                String deletedValue = currentItem.next.data.value;
+                currentItem.next = currentItem.next.next;
+                head = dummyItem.next;
+                return deletedValue;
+            }
+            currentItem = currentItem.next;
+        }
+        return null;
+    }
+
+    public HashTable.Entry popHead() {
+        if (head == null)
+            return null;
+        HashTable.Entry result = head.data;
+        head = head.next;
+        return result;
+    }
+
+    public void clear() {
+        head = null;
+    }
+
+    public boolean empty() {
+        return head == null;
+    }
+}
