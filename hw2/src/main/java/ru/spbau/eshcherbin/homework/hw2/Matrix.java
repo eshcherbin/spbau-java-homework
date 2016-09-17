8864008b935e60c1e7a8d90befbf713f7c6b490d
Egor Lord_F Shcherbin <javamanse@ya.ru>
2016-09-18 06:15:04 +0700

    Added Matrix class and spiral subtask

--- hw2/src/main/java/ru/spbau/eshcherbin/homework/hw2/Matrix.java
+++ hw2/src/main/java/ru/spbau/eshcherbin/homework/hw2/Matrix.java
@@ -1,5 +1,8 @@
 package ru.spbau.eshcherbin.homework.hw2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * A square matrix of integers
  */
@@ -47,5 +50,33 @@ public class Matrix {
 
     public void setAt(int i, int j, int value) {
         matrix[i][j] = value;
+    }
+
+    /**
+     * Returns the matrix contents along the clockwise spiral from the center
+     * Size should be odd
+     * @return ArrayList that contains the elements along the spiral if matrix's size is odd, null otherwise
+     */
+    public ArrayList<Integer> elementsAlongSpiral() {
+        if (matrix.length % 2 == 0) {
+            return null;
+        }
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int step = 0; step <= matrix.length / 2; step++) {
+            for (int i = step; i < matrix.length - step; i++) {
+                result.add(matrix[step][i]);
+            }
+            for (int i = step + 1; i < matrix.length - step; i++) {
+                result.add(matrix[i][matrix.length - step - 1]);
+            }
+            for (int i = matrix.length - step - 2; i >= step; i--) {
+                result.add(matrix[matrix.length - step - 1][i]);
+            }
+            for (int i = matrix.length - step - 2; i > step; i--) {
+                result.add(matrix[i][step]);
+            }
+        }
+        Collections.reverse(result);
+        return result;
     }
 }
--- hw2/src/test/java/ru/spbau/eshcherbin/homework/hw2/MatrixTest.java
+++ hw2/src/test/java/ru/spbau/eshcherbin/homework/hw2/MatrixTest.java
@@ -1,9 +1,16 @@
 package ru.spbau.eshcherbin.homework.hw2;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class MatrixTest {
-    @org.junit.Test
+
+    @Test
     public void testCopyConstructor() throws Exception {
         int[][] array = {
                 {1, 2, 3},
@@ -19,7 +26,7 @@ public class MatrixTest {
         }
     }
 
-    @org.junit.Test
+    @Test
     public void testSizeConstructor() throws Exception {
         Matrix matrix = new Matrix(10);
         for (int i = 0; i < matrix.getSize(); i++) {
@@ -29,7 +36,7 @@ public class MatrixTest {
         }
     }
 
-    @org.junit.Test(expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testCopyConstructorFail() throws Exception {
         int[][] array = {
                 {1, 2, 3},
@@ -39,8 +46,29 @@ public class MatrixTest {
         Matrix matrix = new Matrix(array);
     }
 
-    @org.junit.Test(expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testSizeConstructorFail() throws Exception {
         Matrix matrix = new Matrix(0);
+    }
+
+    @Test
+    public void elementsAlongSpiral() throws Exception {
+        int[][] array = {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25},
+        };
+        Matrix matrix = new Matrix(array);
+        ArrayList<Integer> spiralElements = matrix.elementsAlongSpiral();
+        List<Integer> desiredSpiralElements = Arrays.asList(
+                13, 12, 17, 18, 19,
+                14, 9, 8, 7, 6,
+                11, 16, 21, 22, 23,
+                24, 25, 20, 15, 10,
+                5, 4, 3, 2, 1
+        );
+        assertEquals(desiredSpiralElements, spiralElements);
     }
 }
