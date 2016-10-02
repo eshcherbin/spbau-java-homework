8bb731242aae2df33f230d3968767206c3a916db
Egor Lord_F Shcherbin <javamanse@ya.ru>
2016-10-03 03:12:19 +0700

    Implemented a tree set, no tests and comments yet

--- hw4/src/java/ru/spbau/eshcherbin/homework/hw4/MyTreeSet.java
+++ hw4/src/java/ru/spbau/eshcherbin/homework/hw4/MyTreeSet.java
@@ -3,7 +3,7 @@ package ru.spbau.eshcherbin.homework.hw4;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class MyTreeSet<T extends Comparable> {
+public class MyTreeSet<T extends Comparable<T>> {
 
     private int size;
 
@@ -20,20 +20,64 @@ public class MyTreeSet<T extends Comparable> {
         }
         TreeNode currentNode = root;
         while (true) {
-            if (currentNode.data == element) {
+            int compareResult = element.compareTo(currentNode.data);
+            if (compareResult == 0) {
                 return false;
+            } else if (compareResult < 0) {
+                if (currentNode.leftSon == null) {
+                    currentNode.leftSon = new TreeNode(element);
+                    return true;
+                } else {
+                    currentNode = currentNode.leftSon;
+                }
+            } else {
+                if (currentNode.rightSon == null) {
+                    currentNode.rightSon = new TreeNode(element);
+                    return true;
+                }
+                else {
+                    currentNode = currentNode.rightSon;
+                }
             }
-            if (currentNode.)
         }
-        return true;
+    }
+
+    public boolean contains(@NotNull T element) {
+        if (root == null) {
+            return false;
+        }
+        TreeNode currentNode = root;
+        while (true) {
+            int compareResult = element.compareTo(currentNode.data);
+            if (compareResult == 0) {
+                return true;
+            } else if (compareResult < 0) {
+                if (currentNode.leftSon == null) {
+                    return false;
+                } else {
+                    currentNode = currentNode.leftSon;
+                }
+            } else {
+                if (currentNode.rightSon == null) {
+                    return false;
+                }
+                else {
+                    currentNode = currentNode.rightSon;
+                }
+            }
+        }
+    }
+
+    public int size() {
+        return size;
     }
 
     private class TreeNode {
-        private T data;
-        private TreeNode leftSon;
-        private TreeNode rightSon;
+        private @NotNull T data;
+        private @Nullable TreeNode leftSon;
+        private @Nullable TreeNode rightSon;
 
-        public TreeNode(T data) {
+        public TreeNode(@NotNull T data) {
             this.data = data;
             leftSon = null;
             rightSon = null;
